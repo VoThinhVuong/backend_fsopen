4d646fc1720b90d19ce6239de4a60831972a10b4
VoThinhVuong <127602753+VoThinhVuong@users.noreply.github.com>
2025-02-12 00:01:00 +0700

    3.2 done

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { request } = require('http')
+const app = express()
+
+let persons = [
+    { 
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
+let ppl = persons.length == 1 ? "person" : "people"
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phone book has info for ${persons.length} ${ppl}</p>
+                   <p>${Date()}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
